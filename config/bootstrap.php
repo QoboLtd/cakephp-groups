e34010f2ef3a9a0cd5b15568b164ee6af3bdd837
George Constantinou <g.constantinou@qobo.biz>
2016-11-21 20:13:50 +0200

    write configuration parameter for default group (task #3088)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,7 +1,14 @@
 <?php
+use Cake\Core\Configure;
 use Cake\Event\Event;
 use Cake\Event\EventManager;
 use Cake\ORM\TableRegistry;
+
+Configure::write('Groups.defaultGroup', [
+    'name' => 'Everyone',
+    'deny_edit' => 1,
+    'deny_delete' => 1
+]);
 
 EventManager::instance()->on(
     'Model.afterSaveCommit',
@@ -10,7 +17,9 @@ EventManager::instance()->on(
         if ('CakeDC/Users.Users' === $event->subject()->registryAlias()) {
             $table = TableRegistry::get('Groups.Groups');
             // get default group
-            $group = $table->findByNameAndDenyDeleteAndDenyEdit('Everyone', 1, 1)->contain('Users')->first();
+            $group = $table->find('all', [
+                'conditions' => Configure::read('Groups.defaultGroup')
+            ])->first();
             // need to re-fetch the user entity from the database
             // as is still considered as new and link() fails.
             $user = $table->Users->get($entity->id);
