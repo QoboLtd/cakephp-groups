07600261669a4589408e3b8c1e3a08390b2666b9
George Constantinou <g.constantinou@qobo.biz>
2016-11-22 12:32:34 +0200

    added import task on Groups console shell (task #3088)

--- src/Shell/GroupShell.php
+++ src/Shell/GroupShell.php
@@ -10,7 +10,8 @@ class GroupShell extends Shell
      * {@inheritDoc}
      */
     public $tasks = [
-        'Groups.Assign'
+        'Groups.Assign',
+        'Groups.Import'
     ];
 
     /**
@@ -19,11 +20,16 @@ class GroupShell extends Shell
     public function getOptionParser()
     {
         $parser = parent::getOptionParser();
+
         $parser
             ->description('Qobo Groups Shell that handle\'s related tasks.')
             ->addSubcommand(
                 'assign',
                 ['help' => 'Assign group to all users.', 'parser' => $this->Assign->getOptionParser()]
+            )
+            ->addSubcommand(
+                'import',
+                ['help' => 'Import system groups.', 'parser' => $this->Import->getOptionParser()]
             );
 
         return $parser;
