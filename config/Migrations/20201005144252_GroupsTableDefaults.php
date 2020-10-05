029f4ed84ba2f90b437b62a6aebdb4873e93a9f6
Nicos Panayides <nicosp@gmail.com>
2020-10-05 14:53:19 +0300

    Added migration for defaults

---
+++ config/Migrations/20201005144252_GroupsTableDefaults.php
@@ -0,0 +1,18 @@
+<?php
+use Migrations\AbstractMigration;
+
+class RenameGroupsTable extends AbstractMigration
+{
+
+    /**
+     * Add defaults to deny_delete and deny_edit
+     *
+     */
+    public function change()
+    {
+        $this->table('qobo_groups')
+            ->changeColumn('deny_delete', 'boolean', ['default' => false ])
+            ->changeColumn('deny_edit', 'boolean', ['default' => false ])
+            ->save();
+    }
+}
